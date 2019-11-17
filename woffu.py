5d6d1748150dfcec4ee683ab1302b9eb8fefcd09
Daniel Hervás <dhervas@outlook.es>
2019-11-17 12:20:10 +0100

    Clean up and reorder code (untested)

--- woffu.py
+++ woffu.py
@@ -1,70 +1,82 @@
 import requests
 import json
 import os.path
-# import pytz
 from datetime import datetime
 from operator import itemgetter
 
+# aux functions
+def getAuthHeaders(username, password):
+    # we need to get the Bearer access token for every request we make to Woffu
+    print("Getting access token...\n")
+    access_token = requests.post(
+        "https://app.woffu.com/token",
+        data = f"grant_type=password&username={username}&password={password}"
+    ).json()['access_token']
+    return {
+        'Authorization': 'Bearer ' + access_token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=utf-8'
+    }
+
+def getDomainUserCompanyId(auth_headers):
+    # This function should only be called the first time the script runs.
+    # We'll store the results for subsequent executions
+    print("Getting IDs...\n")
+    users = requests.get(
+        "https://app.woffu.com/api/users", 
+        headers = auth_headers
+    ).json()
+    company = requests.get(
+        f"https://app.woffu.com/api/companies/{users['CompanyId']}", 
+        headers = auth_headers
+    ).json()
+    return company['Domain'], users['UserId'], users['CompanyId']
+
 print("Woffu Autologin Script\n")
 if os.path.exists("./data.json"):
+    has_credentials = True
+if (has_credentials):
     with open("./data.json", "r") as json_data:
         login_info = json.load(json_data)
-        # domain, username, password, user_id, signed_in = itemgetter(
-        signed_in, password, username = itemgetter(
-            # "domain",
-            "signed_in",
-            "password",
-            # "user_id",
+        domain, username, password, user_id, signed_in, company_id = itemgetter(
+            "domain",
             "username"
+            "password",
+            "user_id",
+            "signed_in",
+            "company_id",
         )(login_info)
 else:
     username = input("Enter your username:\n")
     password = input("Enter your password:\n")
-    # insert woffu request
-    data = {
-        "username": username,
-        "password": password,
-        "signed_in": False
-    }
-    with open("data.json", "w") as login_info:
-        json.dump(data, login_info)
 
-token_request_data_string = f"grant_type=password&username={username}&password={password}"
-token_request = requests.post(
-    "https://app.woffu.com/token",
-    data = token_request_data_string
-)
-access_token = token_request.json()['access_token']
-auth_headers = {
-    'Authorization': 'Bearer ' + access_token,
-    'Accept': 'application/json',
-    'Content-Type': 'application/json;charset=utf-8'
-    # 'Content-Type': 'application/json'
-}
+auth_headers = getAuthHeaders()
 
-users = requests.get(
-    "https://app.woffu.com/api/users", 
-    headers = auth_headers
-).json()
-user_id = users['UserId']
-company_id = users['CompanyId']
+if (not has_credentials):
+    domain, user_id, company_id = getDomainUserCompanyId(auth_headers)
 
-company = requests.get(
-    f"https://app.woffu.com/api/companies/{company_id}", 
-    headers = auth_headers
-).json()
-domain = company['Domain']
-
+print("Sending sign request...\n")
 signin = requests.post(
     f"https://{domain}/api/svc/signs/signs",
     json = {
-        # 'DeviceId': 'WebApp',
         'StartDate': datetime.now().replace(microsecond=0).isoformat()+"+01:00",
         'EndDate': datetime.now().replace(microsecond=0).isoformat()+"+01:00",
         'TimezoneOffset': "-60",
         'UserId': user_id
     },
     headers = auth_headers
-)
+).status_code
 
-breakpoint
+if (not has_credentials):
+    with open("data.json", "w") as login_info:
+        json.dump(
+            {
+                "username": username,
+                "password": password,
+                "signed_in": True,
+                "user_id": user_id,
+                "company_id": company_id,
+                "domain": domain
+            },
+            login_info
+        )
